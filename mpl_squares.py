e593b9bbdc48150188864e0d9766648b87517b1e
zahar <zaharambrozyak@gmail.com>
2021-07-03 19:00:59 +0300

    1.1

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+squares = [x*x for x in range(1,101)]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# Задати назву для графіка та кожної з осей.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Задати розмір шрифту для підписів на розмітці.
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
