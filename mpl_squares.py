3de6b387c10b05a31e7da874fdbc2cdf3ad8190f
zahar <zaharambrozyak@gmail.com>
2021-07-03 19:29:48 +0300

    Finish project

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-input_values = range(1,11)
-squares = [x*x for x in range(1,11)]
+x_values = range(1,1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.cool, s=10)
 
 # Задати назву для графіка та кожної з осей.
 ax.set_title("Square Numbers", fontsize=24)
@@ -13,6 +13,11 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Задати розмір шрифту для підписів на розмітці.
-ax.tick_params(axis='both', labelsize=14)
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Задати діапазон для кожної з осей.
+ax.axis([0, 1100, 0, 1100000])
+
+plt.savefig('squares_plot.png', bbox_inches='tight')
 
 plt.show()
